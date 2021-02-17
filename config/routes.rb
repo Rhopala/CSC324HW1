eb8abf53b907fb7b83f1bcaa99d103b80aa8e28a
Xuanhe Chen <mqdzd118119@gmail.com>
2021-02-17 17:07:00 -0600

    HW7 finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,8 @@
   get 'static_pages/home'
   get 'static_pages/help'
   get 'static_pages/about'
-  resources :creatures
+ # resources :creatures
+  resources :users
 
 # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -21,12 +21,6 @@ ActiveRecord::Schema.define(version: 2021_02_17_204929) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table "maindishes", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
