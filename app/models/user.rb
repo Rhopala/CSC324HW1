a68ac0fdd8dba43408494e44102c1a544fa8b6bb
Xuanhe Chen <mqdzd118119@gmail.com>
2021-02-17 15:06:49 -0600

    Followed most of Chapter6

---
+++ app/models/user.rb
@@ -0,0 +1,6 @@
+class User < ApplicationRecord
+validates :email, presence: true, length: { maximum: 255 }
+validates :name, presence: true, length: { maximum: 50}
+has_secure_password
+validates :password, presence: true, length: { minimum: 6 }
+end
